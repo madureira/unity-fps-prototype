5aa51f3c99babdda5d213ccdaa8361b06c128628
Rafael Madureira <rafael.madureira@wildlifestudios.com>
2021-03-13 16:58:41 -0300

    Add animated enemy with basic A.I.

---
+++ Assets/Scripts/Looker.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class Looker : MonoBehaviour
+{
+    public GameObject enemy;
+    public float activeTimeInSeconds = 5;
+    private bool _movingDown;
+    private float _activeTime;
+    private Zombie _zombie;
+    private SphereCollider _sphereCollider;
+
+    private void Start()
+    {
+        _zombie = enemy.GetComponent<Zombie>();
+        _sphereCollider = GetComponent<SphereCollider>();
+    }
+
+    private void Update()
+    {
+        if (_movingDown == false)
+        {
+            transform.position -= new Vector3(0.1f, 0, 0.1f);
+        }
+        else
+        {
+            transform.position += new Vector3(0.1f, 0, 0.1f);
+        }
+
+        if (transform.position.z > 10)
+        {
+            _movingDown = false;
+        }
+        else if (transform.position.z < -10)
+        {
+            _movingDown = true;
+        }
+
+        _activeTime -= Time.deltaTime;
+
+        if (_activeTime > 0)
+        {
+            return;
+        }
+
+        _zombie.enabled = false;
+        //_sphereCollider.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag != "Player")
+        {
+            return;
+        }
+
+        _zombie.enabled = true;
+        _activeTime = activeTimeInSeconds;
+        //_sphereCollider.enabled = false;
+    }
+}
---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour
+{
+    public GameObject player;
+    private NavMeshAgent _navMesh;
+
+    private void Start()
+    {
+        _navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _navMesh.destination = player.transform.position;
+    }
+    
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            Debug.LogWarning("Peguei!!!!");
+        }
+    }
+}
